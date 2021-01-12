b7d800106d300944fd1c0b7d6978e809456a2b60
Christian <christian.gernsoe@gmail.com>
2021-01-12 11:17:24 +0100

    Finished response logic and roomHandler

--- jSpace2020/jSpace-Project/src/common/src/main/Server.java
+++ jSpace2020/jSpace-Project/src/common/src/main/Server.java
@@ -1,6 +1,5 @@
-package common.src.main;
-
 import org.jspace.*;
+
 	/*
 	 *  Server checks that the name is valid (not empty, unique and not too long) 
 	 *  and creates a new room, if the roomID doesn't exist already (Lobby/butler method)
@@ -25,8 +24,8 @@ public class Server {
 		
 		Space lobby = new SequentialSpace();
 		
-		host = "tcp://127.0.0.1:9001/";
-		repoURI = host + "?keep";
+		String host = "tcp://127.0.0.1:9001/";
+		String repoURI = host + "?keep";
 		
 		repo.add("lobby", lobby);
 		repo.addGate(repoURI);
@@ -34,46 +33,65 @@ public class Server {
 		
 		Space rooms = new SequentialSpace();
 			
-		private int roomCounter = 0;
+        int roomCounter = 0;
+        
+        // id=1 rooms(id=1,counter=0)
+        // id=0 rooms(id=0,counter=1)
+        // id=0 rooms(id=0,counter=1)
 		
 		String roomURI;
 		
 		while(true) {
 			System.out.println("Waiting for client to enter...");
 			
-			Object[] credentials = lobby.get(new ActualField("enter"), new FormalField(String.class), new FormalField(String.class));
-			String who = credentials[1];
-			String roomID = credentials[2];
+			Object[] credentials = lobby.get(new ActualField("enter"), new FormalField(String.class), new FormalField(Integer.class));
+			String who = (String) credentials[1];
+			int roomID = (int) credentials[2];
 			
 			System.out.println(who + " wants to join room: " + roomID);
 			
-			Object[] getRoom = rooms.queryp(new ActualField("roomURI"), new FormalField(String.class), 
-										  new ActualField(roomID), new FormalField(String.class));
+			Object[] getRoom = rooms.queryp(new ActualField(roomID), new FormalField(Integer.class));
 			if (getRoom != null) {
-				roomURI = getRoom[3];
+                roomURI = host + "game" + (int) getRoom[1] + "?keep";   // fx. tcp://127.0.0.1:9001/game0?keep
 			} else {
 				System.out.println("Creating new room with ID: " + roomID + " for: " + who);
-				
-				roomURI = host + roomCounter + "?keep"
-						
-				rooms.put("roomURI", who, roomID, roomURI);
-				new Thread(new roomHandler(roomID, roomURI)).start();
-			}
+				roomURI = host + "game" + roomCounter + "?keep";        // fx. tcp://127.0.0.1:9001/game0?keep
+				rooms.put(roomID, roomCounter);
+                new Thread(new roomHandler(roomID, roomCounter, roomURI, repo)).start();
+                roomCounter++;
+            }
+            
+            System.out.println("Sending user: " + who + " to game room: " + roomCounter);
+            lobby.put("roomURI", who, roomID, roomURI);
 		}
 	}
 }
 
 class roomHandler implements Runnable {
-	private int roomID;
+    private int roomID;
+    private int roomCounter;
 	private String roomURI;
-	private Space mySpace;
+    private Space gameRoom;
+    private SpaceRepository repo;
 
-	public roomHandler(int RoomID, String roomURI) {
-		this.roomID = roomID;
-		this.roomURI = roomURI;
-	}
-	
-	public static void run() {
-		
-	}
+	public roomHandler(int roomID, int roomCounter, String roomURI, SpaceRepository repo) {
+        this.roomID = roomID;
+        this.roomCounter = roomCounter;
+        this.roomURI = roomURI;
+        this.repo = repo;
+    }
+    
+	public void run() {
+        gameRoom = new SequentialSpace(); 
+        repo.add("game" + roomCounter, gameRoom);
+
+        while(true) {
+            try {
+                gameRoom.get(new ActualField("ready"));
+                
+            } catch (InterruptedException e) {
+                System.out.println(e.getStackTrace());
+            }
+        }
+    }
 }
