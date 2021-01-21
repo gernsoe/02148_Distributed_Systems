366d36c6058fbaba1d44026a5618eec16ba00192
Clair <clairshecodes@gmail.com>
2021-01-21 14:43:58 +0100

    level label

--- jSpace2020/jSpace-Project/src/common/src/main/GUI/src/WinBuilder/endScreen.java
+++ jSpace2020/jSpace-Project/src/common/src/main/GUI/src/WinBuilder/endScreen.java
@@ -123,12 +123,12 @@ public class endScreen {
 		// "Level" text
 		
 		JLabel atLevel = new JLabel("New label");
-		atLevel.setBounds(128, 293, 61, 16);
+		atLevel.setBounds(128, 288, 61, 16);
 		panel.add(atLevel);
 		
 		JLabel levelLabel = new JLabel("");
 		levelLabel.setBounds(128, 261, 61, 60);
-		Image img1 = new ImageIcon(this.getClass().getResource("/level.png")).getImage();
+		Image img1 = new ImageIcon(this.getClass().getResource("/imgLevel.png")).getImage();
 		levelLabel.setIcon(new ImageIcon(img1));
 		panel.add(levelLabel);
 		
